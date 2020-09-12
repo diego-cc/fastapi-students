b0aae36922a6df1e9142566b00298429f0f92948
Diego C <diego.cc.github@gmail.com>
2020-09-12 18:54:49 +0800

    updated charset

--- app/db.py
+++ app/db.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://students_admin:Password1@localhost/fastapi_students'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://students_admin:Password1@localhost/fastapi_students?charset=utf8mb4'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
